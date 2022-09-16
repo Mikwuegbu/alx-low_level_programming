255d83e4455838cbe73e4cdab5caf3247f421ddd
Mark Stephen <mstephen130@yahoo.com>
2022-09-16 14:19:35 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - prints to string
+ *
  * Return: 1 not successful
  */
+
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
