35dc3c92f4a178b49ba9d0ffbec02ca841a88297
Mark Stephen <mstephen130@yahoo.com>
2022-09-16 14:14:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - prints to string
  * Return: 1 not successful
  */
-
 int main(void)
 {
-
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
-
-}
